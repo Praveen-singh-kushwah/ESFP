66e7c1ee971ae91ef68821e5153031fbb9063667
Praveen-singh-kushwah <98682189+Praveen-singh-kushwah@users.noreply.github.com>
2022-02-07 08:14:54 +0530

    Update contact management system.c

--- contact management system.c
+++ contact management system.c
@@ -34,7 +34,6 @@ main:
 
     system("cls");    /* ************Main menu ***********************  */
     system("color 3f");
-    printf("\n\t **** Knowledge 360 ****");
     printf("\n\t **** Welcome to Contact Management System ****");
     printf("\n\n\n\t\t\tMAIN MENU\n\t\t=====================\n\t\t[1] Add a New Contact\n\t\t[2] List all Contacts\n\t\t[3] Search for Contact\n\t\t[4] Edit a Contact\n\t\t[5] Delete a Contact\n\t\t[0] Exit\n\t\t=================\n\t\t");
 
